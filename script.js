4ccb24c3003ea7d886e3b56653cbb7b6647b2e7f
Joao Caio <jocaio@uol.com.br>
2021-11-08 18:33:49 -0300

    Aulas Strings

---
+++ script.js
@@ -0,0 +1,83 @@
+let nome = 'João Caio Lopes Bernardino';
+
+let resultado = nome.split(' ');
+
+console.log (resultado)
+console.log (resultado[3])
+
+
+
+
+/*
+Terceira aula, trim, remove espaços desnecessários
+let nome = '      João   ';
+
+let resultado = nome.trim().length;
+
+console.log (resultado)*/
+
+
+
+
+/*
+Terceira aula, toUpperCase
+let nome = 'João Caio Lopes Bernardino';
+
+let resultado = nome.toUpperCase()
+
+console.log (resultado) */
+
+
+
+
+
+
+
+/*
+Terceira aula, "replace"
+
+let nome = 'João Caio Lopes Bernardino';
+
+let resultado = nome.replace('Lopes', 'Silva');
+
+console.log (resultado)
+console.log (nome) */
+
+
+
+
+
+
+/*
+Segunda aula string, "substr"
+
+let nome = 'João Caio Lopes Bernardino';
+
+let resultado = nome.substr(-4, 2);
+
+console.log (resultado);*/
+
+
+
+
+
+
+
+
+
+/*
+Primeiro exemplo, idexOf
+
+let nome = 'João Caio Lopes Bernardino';
+
+let resultado = '';
+
+if (nome.indexOf('Lopes') > -1) {
+        resultado = "Achou!";
+} else {
+    resultado = "Não achou!";
+}
+
+console.log ( resultado );
+*/
+
